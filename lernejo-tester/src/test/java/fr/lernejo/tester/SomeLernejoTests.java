7a8c1ae302ad548cf788a4d51c52306fbef172e4
PierreBls <pierre.blais@epita.fr>
2021-06-17 13:08:11 +0200

    partie 4

---
+++ lernejo-tester/src/test/java/fr/lernejo/tester/SomeLernejoTests.java
@@ -0,0 +1,16 @@
+package fr.lernejo.tester;
+
+import fr.lernejo.tester.api.TestMethod;
+
+public class SomeLernejoTests {
+    @TestMethod
+    public void ok(){
+    }
+
+    @TestMethod
+    public void ko(){
+    }
+    public void none(){
+        throw new java.lang.IllegalStateException();
+    }
+}
